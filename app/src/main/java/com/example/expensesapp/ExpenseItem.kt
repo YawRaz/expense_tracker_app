8918c1fe5875c6b55fd1f7437765897d36042217
YawRaz <riclerico@gmail.com>
2024-04-23 18:01:02 +0000

    feat (layouts) generate add item activity layout

--- app/src/main/java/com/example/expensesapp/ExpenseItem.kt
+++ app/src/main/java/com/example/expensesapp/ExpenseItem.kt
@@ -1,4 +1,11 @@
 package com.example.expensesapp
 
-class ExpenseItem {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpenseItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_item)
+    }
 }
--- app/src/main/java/com/example/expensesapp/ItemsAdapter.kt
+++ app/src/main/java/com/example/expensesapp/ItemsAdapter.kt
@@ -1,4 +1,4 @@
 package com.example.expensesapp
 
-class ItemsAdapter {
+class ItemsAdapter:  {
 }
